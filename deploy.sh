e4e2108c7d5a95c66d7114311229843456ddc895
fepfitra <fitriansyahekaputra2001@mail.ugm.ac.id>
2024-12-11 14:09:09 +0700

    cicd: returns domain after deploy

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,4 @@ podman push asia-southeast2-docker.pkg.dev/capstone-project-c242-ps363/backend/i
 gcloud run deploy inference-model --image=asia-southeast2-docker.pkg.dev/capstone-project-c242-ps363/backend/inference-model --platform=managed --region=asia-southeast2 --allow-unauthenticated --memory=1Gi
 
 # update nginx
-url=$(gcloud run services describe inference-model --platform=managed --region=asia-southeast2 --format='value(status.url)')
-url=${url#https://}
-echo "setup $url to nginx"
-gcloud compute ssh nginx -- -p 22 "sudo sed 's/proxy_pass.*app;/proxy_pass https:\/\/$url;/g' -i /etc/nginx/conf.d/signlang.conf; sudo nginx -t; sudo systemctl reload nginx"
+gcloud run services describe inference-model --platform=managed --region=asia-southeast2 --format='value(status.url)'
